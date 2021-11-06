44236454522e99c911a3829a3b3f1dfff8dfe68e
Naufal Adriel Fauzi <fnaufaladriel@gmail.com>
2021-11-07 06:07:28 +0700

    js introduction

---
+++ js/convert.js
@@ -0,0 +1,15 @@
+
+let a=prompt("masukan nilai:");
+    if ((a>=85)&&(a<=100)) 
+        alert("A");
+    else if((a>=70)&&(a<=84))
+        alert("B");
+    else if((a>=50)&&(a<=69))
+        alert("C");
+    else if((a>=40)&&(a<=49))
+        alert("D");
+    else if ((a>=0)&&(a<=39))
+        alert("E");
+    else    
+        alert("nilai tidak valid.");
+    
